6746669d7731c7bad1c5c719c5893dc131864f68
macray <macraykorir254@gmail.com>
2023-08-14 21:20:00 +0300

    Completed Assignment

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+# # lib/app.py
+# print("Hello world!")
+# print("Hello sun!")
+# print("Hello sky!")
+
+# lib/app.py Choosing print() Ending: You can control the ending character of the print() statement using the end parameter.
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
